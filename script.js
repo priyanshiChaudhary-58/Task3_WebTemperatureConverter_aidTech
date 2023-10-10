82047e760192e6414554d2c97fae807bfa0c6496
Priyanshi Kumari <117848661+priyanshiChaudhary-58@users.noreply.github.com>
2023-10-10 21:45:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+function convertTemperature() {
+    const temperatureInput = parseFloat(document.getElementById("temperatureInput").value);
+    const fromUnit = document.getElementById("fromUnit").value;
+    const toUnit = document.getElementById("toUnit").value;
+
+    let convertedTemperature;
+
+    if (fromUnit === "celsius") {
+        if (toUnit === "fahrenheit") {
+            convertedTemperature = (temperatureInput * 9/5) + 32;
+        } else if (toUnit === "kelvin") {
+            convertedTemperature = temperatureInput + 273.15;
+        } else {
+            convertedTemperature = temperatureInput;
+        }
+    } else if (fromUnit === "fahrenheit") {
+        if (toUnit === "celsius") {
+            convertedTemperature = (temperatureInput - 32) * 5/9;
+        } else if (toUnit === "kelvin") {
+            convertedTemperature = (temperatureInput - 32) * 5/9 + 273.15;
+        } else {
+            convertedTemperature = temperatureInput;
+        }
+    } else if (fromUnit === "kelvin") {
+        if (toUnit === "celsius") {
+            convertedTemperature = temperatureInput - 273.15;
+        } else if (toUnit === "fahrenheit") {
+            convertedTemperature = (temperatureInput - 273.15) * 9/5 + 32;
+        } else {
+            convertedTemperature = temperatureInput;
+        }
+    }
+
+    document.getElementById("convertedTemperature").textContent = convertedTemperature.toFixed(2);
+}
